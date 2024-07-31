dedf45055b8b4ca16c335261561f31599d98476f
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2024-07-31 14:57:47 +0200

    empty blacklist. its optimization only, and we are to lazy to maintain it.

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd.adjustment/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/adjustment/qvto/util/QVToModelCache.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd.adjustment/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/adjustment/qvto/util/QVToModelCache.java
@@ -17,9 +17,6 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreSwitch;
-import org.eclipse.gmf.runtime.notation.NotationPackage;
-import org.modelversioning.emfprofile.EMFProfilePackage;
-import org.modelversioning.emfprofileapplication.EMFProfileApplicationPackage;
 import org.palladiosimulator.analyzer.workflow.ConstantsContainer;
 import org.palladiosimulator.analyzer.workflow.jobs.LoadSharedPCMLibrariesIntoBlackboard;
 
@@ -30,52 +27,50 @@ import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
  * This cache implementation is used to store models (e.g., PCM models or runtime measurement
  * models) that can be parameters of QVTo transformations. To store a model in the cache, its
  * corresponding {@link EPackage} (its meta-model) is used as tag.
- * 
+ *
  * @author Florian Rosenthal, Sebastian Krach, Julijan Katic
  *
  */
 public class QVToModelCache {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(QVToModelCache.class);
-	
+
 	// Put (statically) EClass objects of blackboard models that are not intended to be
 	// transformation parameters here
-	private static final EClass[] MODEL_ECLASS_BLACKLIST = {
-			EMFProfileApplicationPackage.eINSTANCE.getProfileApplication(), EMFProfilePackage.eINSTANCE.getProfile(),
-			NotationPackage.eINSTANCE.getDiagram() };
-	
+	private static final EClass[] MODEL_ECLASS_BLACKLIST = {};
+
 	// switch to determine the meta-model/EPackage of a model
 	private static final EcoreSwitch<EPackage> MODELTYPE_RETRIEVER = new EcoreSwitch<EPackage>() {
-		
+
 		@Override
 		public EPackage caseEPackage(final EPackage ePackage) {
 			return ePackage; // we found the model type, just return
 		}
-		
+
 		@Override
 		public EPackage caseEClass(final EClass eClass) {
 			return doSwitch(eClass.eContainer());
 		}
-		
+
 		@Override
 		public EPackage defaultCase(final EObject eObject) {
 			return doSwitch(eObject.eClass());
 		}
-		
+
 	};
-	
+
 	private final Map<EPackage, Set<EObject>> cache;
 	private final MDSDBlackboard blackboard;
-	
+
 	public QVToModelCache(final MDSDBlackboard blackboard) {
 		this.cache = new HashMap<>();
 		this.blackboard = Objects.requireNonNull(blackboard);
 		storeBlackboardModels();
 	}
-	
+
 	/**
 	 * Copy constructor
-	 * 
+	 *
 	 * @param from The instance to copy from.
 	 */
 	private QVToModelCache(final QVToModelCache from) {
@@ -85,37 +80,37 @@ public class QVToModelCache {
 						   .flatMap(Collection::stream)
 						   .forEach(this::storeModel);
 	}
-	
+
 	/**
 	 * Stores the given model in the cache. If a model of the same type (denoted by the meta-model,
 	 * i.e., the {@link EPackage} that corresponds to the model) is already present, it will be
 	 * overwritten. <br>
 	 * In case {@code null} is passed, this method does nothing.
-	 * 
+	 *
 	 * @param modelInstance representing a model.
 	 */
 	public void storeModel(final EObject modelInstance) {
 		if (modelInstance != null) {
 			final EPackage metaModel = MODELTYPE_RETRIEVER.doSwitch(modelInstance);
-			
+
 			// We do not want to transform meta models
 			if (modelInstance.equals(metaModel)) {
 				return;
 			}
-			
+
 			this.cache.computeIfAbsent(metaModel, mm -> new HashSet<>())
 					  .add(modelInstance);
 		}
 	}
-	
+
 	/**
 	 * Stores the model found in the partition of the blackboard that is identified by the given id.
 	 * If a model of the same type (denoted by meta-model, i.e., the {@link EPackage} that
 	 * corresponds to the model) is already present, it will be overwritten. <br>
-	 * 
+	 *
 	 * If the given id does not identify the partition, or the requested partition is empty, nothing
 	 * happens.
-	 * 
+	 *
 	 * @param partitionId A string which identifies a {@link ResourceSetPartition} of the global {@link MDSDBlackboard}
 	 * @throws NullPointerException In case {@code partitionId == null}
 	 * @see #storeModel(EObject)
@@ -130,12 +125,12 @@ public class QVToModelCache {
 					.forEach(this::storeModel);
 		}
 	}
-	
+
 	/**
 	 * Removes all of the currently stored models which are instances of the meta-model represented by the
 	 * given ePackage. <br>
 	 * In case {@code null} is passed, this method does nothing.
-	 * 
+	 *
 	 * @param metaModel describes a meta-model
 	 */
 	public void removeModelOfType(final EPackage metaModel) {
@@ -143,11 +138,11 @@ public class QVToModelCache {
 			this.cache.remove(metaModel);
 		}
 	}
-	
+
 	/**
      * Removes the given model from the cache, if present.<br>
      * This method does nothing, if {@code null} is passed or the given model is not cached.
-     * 
+     *
      * @param model
      *            The {@link EObject} to remove from the cache.
      */
@@ -157,7 +152,7 @@ public class QVToModelCache {
 				 .remove(model);
 		}
 	}
-	
+
 	/**
      * Clears the cache, that is, all models are removed.
      */
@@ -168,17 +163,17 @@ public class QVToModelCache {
     /**
      * Creates a snapshot of the current state of the cache.<br>
      * More precisely, this method creates an instance that contains the same models as this one.
-     * 
+     *
      * @return A {@link QVToModelCache} which is a snapshot of the current state of this instance.
      */
     public QVToModelCache snapshot() {
         return new QVToModelCache(this);
     }
-    
+
     /**
      * Gets the currently stored model that is an instance of the meta-model represented by the
      * given ePackage.
-     * 
+     *
      * @param ePackage
      *            An {@link EPackage} that describes a meta-model.
      * @return The model, contained in an {@link Optional} and represented as an {@link EObject},
@@ -199,31 +194,31 @@ public class QVToModelCache {
     				 .flatMap(Collection::stream)
     				 .collect(Collectors.toList());
     }
-    
+
     /**
      * Gets whether a model of the meta-model represented by the given ePackage is currently in
      * store.
-     * 
+     *
      * @param ePackage
      *            An {@link EPackage} that describes a meta-model.
      * @return {@code true} if a model of the given type is stored, {@code false} otherwise.
      * @throws NullPointerException
      *             In case {@code ePackage == null}.
-     * 
+     *
      * @see #getModelsByType(EPackage)
      */
     public boolean containsModelOfType(final EPackage ePackage) {
     	return this.cache.containsKey(Objects.requireNonNull(ePackage));
     }
-    
+
     private void storeBlackboardModels() {
     	assert this.blackboard != null;
     	storeModelFromBlackboardPartition(ConstantsContainer.DEFAULT_PCM_INSTANCE_PARTITION_ID);
     }
-    
+
     private static boolean isBlacklisted(final EObject model) {
     	assert model != null;
-    	
+
     	return model.eResource().getURI().equals(LoadSharedPCMLibrariesIntoBlackboard.PCM_PALLADIO_PRIMITIVE_TYPE_REPOSITORY_URI)
     			|| Arrays.stream(MODEL_ECLASS_BLACKLIST).anyMatch(bannedEClass -> bannedEClass == model.eClass());
     }
