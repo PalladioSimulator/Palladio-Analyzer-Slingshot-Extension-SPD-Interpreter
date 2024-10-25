0bd0facb536a5f1ec838efd4e895eac4ab5444b0
Jens Berberich <jensb@endeavour-laptop>
2024-10-25 14:27:37 +0200

    Improved Logging for Fuzzy Q-Learning

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/FuzzyQLearningModelEvaluator.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/FuzzyQLearningModelEvaluator.java
@@ -20,15 +20,32 @@ public class FuzzyQLearningModelEvaluator extends AbstractFuzzyLearningModelEval
     @Override
     public void update() throws NotEmittableException {
         this.currentState = State.createFromModelAggregators(this);
-        LOGGER.info("Utilization: " + this.currentState.utilization());
-        LOGGER.info("Response time: " + this.currentState.responseTime());
+        final double currentEpsilon = Math.max(Math.exp(-this.epsilon * this.iterationCount), 0.01);
+        LOGGER.info("Utilization: " + this.nf.format(this.currentState.utilization()) + " ("
+                + this.arrayToString(this.currentState.getFuzzyUtil()) + ")" + ", current Epsilon = "
+                + this.nf.format(currentEpsilon));
+        LOGGER.info("Response time: " + this.nf.format(this.currentState.responseTime()) + " ("
+                + this.arrayToString(this.currentState.getFuzzyResponseTime()) + ")");
         if (this.previousState != null) {
             // Step 6: Observe the reinforcement signal r(t + 1) + calculate value for new state
             final double reward = this.calculateReward();
             LOGGER.info("Reward (for the last period): " + reward);
             final double value = this.calculateValueFunction(this.qValues);
+            LOGGER.info("Approximated State Value: " + this.nf.format(value) + ", previously approximated Q-Value "
+                    + this.nf.format(this.approximatedQValue));
             // Step 7: Calculate the error signal
             final double errorSignal = reward + this.discountFactor * value - this.approximatedQValue;
+            LOGGER.info("Old Q-Values: ");
+            for (int wl = 0; wl < 3; wl++) {
+                for (int rt = 0; rt < 3; rt++) {
+                    if (this.previousState.getFiringDegree(wl, rt) > 0) {
+                        LOGGER.info("q-Values for workload " + wl + " and response time " + rt + ": "
+                                + this.arrayToString(this.qValues[wl][rt]) + ", firing degree "
+                                + this.nf.format(this.previousState.getFiringDegree(wl, rt)) + ", partial action "
+                                + (this.partialActions[wl][rt] - 2));
+                    }
+                }
+            }
             // Step 8: Update q-Values
             for (int wl = 0; wl < 3; wl += 1) {
                 for (int rt = 0; rt < 3; rt += 1) {
@@ -36,26 +53,43 @@ public class FuzzyQLearningModelEvaluator extends AbstractFuzzyLearningModelEval
                             * this.previousState.getFiringDegree(wl, rt);
                 }
             }
+            LOGGER.info("Updated Q-Values: ");
+            for (int wl = 0; wl < 3; wl++) {
+                for (int rt = 0; rt < 3; rt++) {
+                    if (this.previousState.getFiringDegree(wl, rt) > 0) {
+                        LOGGER.info("q-Values for workload " + wl + " and response time " + rt + ": "
+                                + this.arrayToString(this.qValues[wl][rt]));
+                    }
+                }
+            }
         }
         // Step 2: Select an action
         this.partialActions = this.choosePartialActions(this.qValues,
-                Math.max(Math.exp(-this.epsilon * this.iterationCount), 0.01));
+                currentEpsilon);
         this.iterationCount++;
         // Step 3: Calculate control action a
         final double a = this.calculateControlAction(this.currentState, this.partialActions);
         // Step 4: Approximate the Q-function
         this.approximatedQValue = this.approximateQFunction(this.currentState, this.partialActions, this.qValues);
         // Logging things + setting various things for next iteration
-        LOGGER.info("Current Q-Values: ");
-        for (int wl = 0; wl < 3; wl++) {
-            for (int rt = 0; rt < 3; rt++) {
-                LOGGER.info("q-Values for workload " + wl + " and response time " + rt + ": "
-                        + this.arrayToString(this.qValues[wl][rt]));
-            }
-        }
         // Step 5: Take action a and let system go to next state (-> in next iteration)
         this.previousAction = (int) Math.round(a);
         this.previousState = this.currentState;
+    }
+
+    @Override
+    double calculateReward() {
+        double reward = super.calculateReward();
+        if (this.previousAction != null) {
+            if (this.previousAction > 0) {
+                // Small penalty for scaling up
+                reward -= this.previousAction * 0.4;
+            } else if (this.previousAction < 0) {
+                // Small reward for scaling down
+                reward -= this.previousAction * 0.2;
+            }
+        }
+        return reward;
     }
 
     @Override
