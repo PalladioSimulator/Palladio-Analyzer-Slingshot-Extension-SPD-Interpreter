b625bca7dec4b7fc2ab4118fc30f9d41bfe48d66
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-11-09 00:51:33 +0100

    Bugfix: Don't use IntervalConstraintFilter for model-based scaling policies

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entities/SPDAdjustorContext.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entities/SPDAdjustorContext.java
@@ -15,7 +15,9 @@ import org.palladiosimulator.analyzer.slingshot.common.events.DESEvent;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.EventHandler;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.Subscriber;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.returntypes.Result;
+import org.palladiosimulator.spd.ModelBasedScalingPolicy;
 import org.palladiosimulator.spd.ScalingPolicy;
+import org.palladiosimulator.spd.constraints.policy.IntervalConstraint;
 import org.palladiosimulator.spd.constraints.target.ThrashingConstraint;
 import org.palladiosimulator.spd.triggers.BaseTrigger;
 import org.palladiosimulator.spd.triggers.ComposedTrigger;
@@ -23,129 +25,135 @@ import org.palladiosimulator.spd.triggers.ScalingTrigger;
 
 /**
  *
- * For each {@code SPDAdjustorContext} there must be at most one Subscriber per
- * EventType.
+ * For each {@code SPDAdjustorContext} there must be at most one Subscriber per EventType.
  *
- * This is mostly relevant for {@link ComposedTrigger}s. For
- * {@link BaseTrigger}, it is not relevant, there is always only one Subscriber
- * for those. However a {@link ComposedTrigger} may consist of multiple triggers
- * with the same stimulus type, which results in multiple subscribers for one
- * type of event, which results in the FilterChain being triggered more often
- * than it should.
+ * This is mostly relevant for {@link ComposedTrigger}s. For {@link BaseTrigger}, it is not
+ * relevant, there is always only one Subscriber for those. However a {@link ComposedTrigger} may
+ * consist of multiple triggers with the same stimulus type, which results in multiple subscribers
+ * for one type of event, which results in the FilterChain being triggered more often than it
+ * should.
  *
  * @author Julijan Katic, Sarah Stieß
  */
 public final class SPDAdjustorContext {
 
-	private static final Logger LOGGER = Logger.getLogger(SPDAdjustorContext.class);
+    private static final Logger LOGGER = Logger.getLogger(SPDAdjustorContext.class);
 
-	private final FilterChain filterChain;
-	private final ScalingPolicy scalingPolicy;
-	private final Set<Subscriber<? extends DESEvent>> associatedHandlers;
+    private final FilterChain filterChain;
+    private final ScalingPolicy scalingPolicy;
+    private final Set<Subscriber<? extends DESEvent>> associatedHandlers;
 
-	private SPDAdjustorState state;
-	private final SPDAdjustorState previousState;
+    private SPDAdjustorState state;
+    private final SPDAdjustorState previousState;
 
-	public SPDAdjustorContext(final ScalingPolicy policy,
-			final Filter triggerChecker,
-			final List<Subscriber.Builder<? extends DESEvent>> associatedHandlers, final TargetGroupState targetGroupState) {
-		this.scalingPolicy = policy;
+    public SPDAdjustorContext(final ScalingPolicy policy, final Filter triggerChecker,
+            final List<Subscriber.Builder<? extends DESEvent>> associatedHandlers,
+            final TargetGroupState targetGroupState) {
+        this.scalingPolicy = policy;
 
-		state = new SPDAdjustorState(policy, targetGroupState);
-		previousState = new SPDAdjustorState(policy, targetGroupState);
+        state = new SPDAdjustorState(policy, targetGroupState);
+        previousState = new SPDAdjustorState(policy, targetGroupState);
 
-		this.filterChain = new FilterChain(this::doOnDisregard, state);
+        this.filterChain = new FilterChain(this::doOnDisregard, state);
 
-		initializeFilterChain(triggerChecker);
+        initializeFilterChain(triggerChecker);
 
-		final PublishResultingEventFilter publisher = new PublishResultingEventFilter();
+        final PublishResultingEventFilter publisher = new PublishResultingEventFilter();
 
+        this.associatedHandlers = associatedHandlers.stream()
+            .map(builder -> builder.handler(publisher))
+            .map(builder -> builder.build())
+            .collect(Collectors.toSet());
+    }
 
-		this.associatedHandlers = associatedHandlers.stream()
-				.map(builder -> builder.handler(publisher))
-				.map(builder -> builder.build())
-				.collect(Collectors.toSet());
-	}
+    /**
+     * Initializes the filter chain.
+     *
+     * The first filter is always a {@link TargetGroupChecker}, the second filter is always a
+     * {@link TriggerChecker}, the last filter is always the {@link Adjustor}. In between are the
+     * Checker for the Constraints.
+     *
+     * @param triggerChecker
+     *            filter to check the policie's {@link ScalingTrigger}.
+     */
+    private void initializeFilterChain(final Filter triggerChecker) {
+        this.filterChain.add(new TargetGroupChecker(this.scalingPolicy.getTargetGroup()));
 
+        this.filterChain.add(triggerChecker);
 
-	/**
-	 * Initializes the filter chain.
-	 *
-	 * The first filter is always a {@link TargetGroupChecker}, the second filter is
-	 * always a {@link TriggerChecker}, the last filter is always the
-	 * {@link Adjustor}. In between are the Checker for the Constraints.
-	 *
-	 * @param triggerChecker filter to check the policie's {@link ScalingTrigger}.
-	 */
-	private void initializeFilterChain(final Filter triggerChecker) {
-		this.filterChain.add(new TargetGroupChecker(this.scalingPolicy.getTargetGroup()));
+        scalingPolicy.getTargetGroup()
+            .getTargetConstraints()
+            .stream()
+            .filter(constraint -> constraint instanceof final ThrashingConstraint thrashingConstraint)
+            .map(constraint -> (ThrashingConstraint) constraint)
+            .forEach(constraint -> this.filterChain
+                .add(AbstractConstraintFilter.createAbstractConstraintFilter(constraint)));
 
-		this.filterChain.add(triggerChecker);
+        scalingPolicy.getPolicyConstraints()
+            .stream()
+            .filter(constraint -> {
+                return !(this.scalingPolicy instanceof ModelBasedScalingPolicy
+                        && constraint instanceof IntervalConstraint);
+            })
+            .forEach(constraint -> this.filterChain
+                .add(AbstractConstraintFilter.createAbstractConstraintFilter(constraint)));
 
+        this.filterChain.add(new Adjustor(this.scalingPolicy));
+    }
 
-		scalingPolicy.getTargetGroup().getTargetConstraints().stream().filter(constraint -> constraint instanceof final ThrashingConstraint thrashingConstraint).map(constraint -> (ThrashingConstraint) constraint).forEach(constraint ->
-				this.filterChain.add(AbstractConstraintFilter.createAbstractConstraintFilter(constraint)));
+    public FilterChain getFilterChain() {
+        return filterChain;
+    }
 
-		scalingPolicy.getPolicyConstraints().forEach(constraint ->
-		this.filterChain.add(AbstractConstraintFilter.createAbstractConstraintFilter(constraint))
-				);
+    public ScalingPolicy getScalingPolicy() {
+        return scalingPolicy;
+    }
 
-		this.filterChain.add(new Adjustor(this.scalingPolicy));
-	}
+    public Collection<Subscriber<? extends DESEvent>> getAssociatedHandlers() {
+        return associatedHandlers;
+    }
 
+    private void doOnDisregard(final Object reason) {
+        this.state = previousState;
+        LOGGER.debug("Filter was not successful: " + reason.toString());
+    }
 
-	public FilterChain getFilterChain() {
-		return filterChain;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.scalingPolicy.getId());
+    }
 
-	public ScalingPolicy getScalingPolicy() {
-		return scalingPolicy;
-	}
+    @Override
+    public boolean equals(final Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other instanceof final SPDAdjustorContext otherContext) {
+            return Objects.equals(this.scalingPolicy.getId(), otherContext.scalingPolicy.getId());
+        }
+        return false;
+    }
 
-	public Collection<Subscriber<? extends DESEvent>> getAssociatedHandlers() {
-		return associatedHandlers;
-	}
+    /**
+     * After all filters are successful, the resulting event should be published.
+     */
+    private class PublishResultingEventFilter implements EventHandler<DESEvent> {
 
-	private void doOnDisregard(final Object reason) {
-		this.state = previousState;
-		LOGGER.debug("Filter was not successful: " + reason.toString());
-	}
+        @Override
+        public Result<?> acceptEvent(final DESEvent event) throws Exception {
+            filterChain.next(event);
+            final FilterResult filterResult = filterChain.getLatestResult();
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.scalingPolicy.getId());
-	}
+            if (filterResult instanceof final FilterResult.Success success) {
+                final Object result = success.nextEvent();
+                LOGGER.debug("Got a result after filtering! " + result.getClass()
+                    .getSimpleName());
 
-	@Override
-	public boolean equals(final Object other) {
-		if (this == other) {
-			return true;
-		}
-		if (other instanceof final SPDAdjustorContext otherContext) {
-			return Objects.equals(this.scalingPolicy.getId(), otherContext.scalingPolicy.getId());
-		}
-		return false;
-	}
+                return Result.of(result);
+            } else {
+                return Result.empty();
+            }
+        }
 
-	/**
-	 * After all filters are successful, the resulting event should be published.
-	 */
-	private class PublishResultingEventFilter implements EventHandler<DESEvent> {
-
-		@Override
-		public Result<?> acceptEvent(final DESEvent event) throws Exception {
-			filterChain.next(event);
-			final FilterResult filterResult = filterChain.getLatestResult();
-
-			if (filterResult instanceof final FilterResult.Success success) {
-				final Object result = success.nextEvent();
-				LOGGER.debug("Got a result after filtering! " + result.getClass().getSimpleName());
-
-				return Result.of(result);
-			} else {
-				return Result.empty();
-			}
-		}
-
-	}
+    }
 }
