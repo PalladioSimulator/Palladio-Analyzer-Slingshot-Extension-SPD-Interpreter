7a463f917edaf5ca1e493507d5a93d56f1136e1f
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2025-01-17 13:23:20 +0100

    remove duplicate SimulationTimeReached events after interpreting the SPD file.

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/SpdBehavior.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/SpdBehavior.java
@@ -2,6 +2,11 @@ package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter;
 
 import static org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.eventcontract.EventCardinality.MANY;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
 import org.palladiosimulator.analyzer.slingshot.common.annotations.Nullable;
 
@@ -60,7 +65,30 @@ public class SpdBehavior implements SimulationBehaviorExtension {
 								    .peek(ac -> LOGGER.debug("AdjustorContext: #handlers = " + ac.getAssociatedHandlers().size()))
 								    .flatMap(ac -> ac.getAssociatedHandlers().stream())
 								    .forEach(driver::registerEventHandler);
+		
+		Collection<SpdBasedEvent> events = result.getEventsToSchedule();
+		
+		removeDuplicates(events);
 
-		return Result.from(result.getEventsToSchedule());
+		return Result.from(events);
+	}
+	
+	private void removeDuplicates(final Collection<SpdBasedEvent> events) {
+		Set<SimulationTimeReached> timeEvents = events.stream()
+				.filter(SimulationTimeReached.class::isInstance)
+				.map(SimulationTimeReached.class::cast)
+				.collect(Collectors.toSet());
+		
+		Set<SimulationTimeReached> rval = new HashSet<>();
+		Set<SimulationTimeReached> duplicates = new HashSet<>();
+		for (SimulationTimeReached event : timeEvents) {
+			if (rval.stream().filter(e -> e.time() == event.time()).findAny().isEmpty()) {
+				rval.add(event);
+			} else {
+				duplicates.add(event);
+			}
+		}
+		
+		events.removeAll(duplicates);
 	}
 }
